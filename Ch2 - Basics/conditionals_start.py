32bfbce65d212a6b47d5e0db50205704b5a0b9e4
Joe Marini <cjmarini68@gmail.com>
2021-10-28 19:24:01 -0700

    Add match-case example

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -13,7 +13,7 @@ def main():
     # conditional statements let you use "a if C else b"
 
     # match-case makes it easy to compare multiple values
-
+    value = "one"
 
 if __name__ == "__main__":
     main()
