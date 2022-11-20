aaf51eb4ef2ea8c25fc2a61f38ca26175ebcbc56
michellecaner <michellecaner@gmail.com>
2022-11-19 18:44:31 -0600

    building hello world

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,11 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    """"This function asks for your name and offers salutations once received"""
+    print("Hello World!")
+    name = input("What is your name?")
+    print("Nice to meet you", name)
 
+if __name__ == "__main__":
+    main()
