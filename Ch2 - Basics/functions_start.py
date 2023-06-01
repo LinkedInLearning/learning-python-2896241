42f7da6da11c513fad8186421e64bc60c3832ebf
minharaza <razamh@mail.uc.edu>
2023-06-01 12:15:21 -0400

    practiceee

--- Ch2 - Basics/functions_start.py
+++ Ch2 - Basics/functions_start.py
@@ -5,17 +5,43 @@
 
 
 # TODO: define a basic function
-
+def func1():
+    print("I am a function")
 
 # TODO: function that takes arguments
-
+def func2(arg1, arg2):
+    print(arg1, " ", arg2)
 
 # TODO: function that returns a value
-
+def cube(x):
+    return x*x*x
 
 # TODO: function with default value for an argument
-
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result = result * num
+    return result
 
 # TODO: function with variable number of arguments
 
+def multiadd(*args):
+    result = 0
+    for x in args:
+        result = result + x
+    return result
+    
 
+func1()
+print(func1())
+print(func1)
+
+func2(3, 2)
+print(func2(3, 2))
+print(cube(3))
+
+print(power(2))
+print(power(2,3))
+print(power(x = 4, num = 2))
+
+print(multiadd(1,2,3,4,5,6))
